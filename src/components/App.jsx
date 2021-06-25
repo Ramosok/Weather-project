c4ab4fe4dc41d219d303d80e463ed7a27efe5b08
Ramosok <ramosok86@gmail.com>
2021-06-25 05:31:34 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+// libraries
+import React, { useState } from 'react';
+// api
+import { getCurrentWeather } from 'api/weather';
+// components
+import Form from 'components/shared/Form';
+import WeatherCard from 'components/WeatherCard';
+// styles
+import './App.css';
+
+const App = () => {
+    const [weatherData, setWeatherData] = useState(null);
+
+    return (
+        <div className="container">
+            <h2 className="page-title">Get Weather for free!</h2>
+            <Form onSubmitCallback={setWeatherData} />
+
+            {weatherData && <WeatherCard weatherData={weatherData} />}
+        </div>
+    );
+};
+
+export default App;
