ded64c38a89540d352dbf9243228253c108423cf
Ramosok <ramosok86@gmail.com>
2021-06-28 06:31:30 +0300

    object is destructured

--- src/components/WeatherCard/index.jsx
+++ src/components/WeatherCard/index.jsx
@@ -2,22 +2,22 @@
 import React from 'react';
 
 const WeatherCard = ({ weatherData }) => {
-
+const {current: {weather_icons,temperature,feelslike,humidity,pressure,visibility},request: {query}} = weatherData;
   return (
     <div className="card_conteiner">
       <div className="page-title"> Weather Card</div>
       <div className="header_card">
-        <p className="card-title"><img src={weatherData.current.weather_icons[0]} alt="icon"/></p>
-        <p className="card-title">{weatherData.request.query}</p>
+        <p className="card-title"><img src={weather_icons[0]} alt="icon"/></p>
+        <p className="card-title">{query}</p>
       </div>
       <div className="left_container">
-        <p className="card-title"><span className="item-title">Temperature:</span><span>{weatherData.current.temperature}℃</span></p>
-        <p className="card-title"><span className="item-title">Feelslike:</span>{weatherData.current.feelslike}℃</p>
+        <p className="card-title"><span className="item-title">Temperature:</span><span>{temperature}℃</span></p>
+        <p className="card-title"><span className="item-title">Feelslike:</span>{feelslike}℃</p>
       </div>
       <div className="right_container">
-        <p className="card-title"><span className="item-title">Humidity:</span>{weatherData.current.humidity}%</p>
-        <p className="card-title"><span className="item-title">Pressure:</span>{weatherData.current.pressure}Pa</p>
-        <p className="card-title"><span className="item-title">Visibility:</span>{weatherData.current.visibility}m</p>
+        <p className="card-title"><span className="item-title">Humidity:</span>{humidity}%</p>
+        <p className="card-title"><span className="item-title">Pressure:</span>{pressure}Pa</p>
+        <p className="card-title"><span className="item-title">Visibility:</span>{visibility}m</p>
       </div>
     </div>
   );
